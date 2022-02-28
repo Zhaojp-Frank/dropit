eea2c7bae3108eca08a64cc37fe8872f0f7f0202
chenjoya <chenjoya@foxmail.com>
2022-02-28 21:38:40 +0800

    Rename some configs

--- tools/check_params.py
+++ tools/check_params.py
@@ -12,16 +12,16 @@ def dist(a, b):
 if __name__ == "__main__":
     writer = SummaryWriter()
     seed_everything(42, workers=True)
-    torch.backends.cudnn.benchmark = True
-    torch.backends.cudnn.deterministic = True
     
-    baseline = load_state_dict("outputs/kinetics400/x3d_s/lightning_logs/version_6/checkpoints/epoch=299-step=278399.ckpt")
-    pg4t = load_state_dict("outputs/kinetics400/temporal_conv2d_epoch=299-step=278399.ckpt")
-    pg4c = load_state_dict("outputs/kinetics400/x3d_s_pg4c_conv1-5+wd_stridex2/lightning_logs/version_0/checkpoints/epoch=299-step=278399.ckpt")
-    pg4s = load_state_dict("outputs/kinetics400/spatail_conv_epoch=299-step=278399.ckpt")
-    for b, t, c, s in zip(baseline, pg4t, pg4c, pg4s):
+    baseline = load_state_dict("outputs/cifar100/resnet18/lightning_logs/version_1/checkpoints/epoch=199-step=77999.ckpt")
+    dropit_fast_minik_08 = load_state_dict("outputs/cifar100/resnet18_fastminkx0.8/lightning_logs/version_1/checkpoints/epoch=199-step=77999.ckpt")
+    dropit_fast_minik_099 = load_state_dict("outputs/cifar100/resnet18_fastminkx0.99/lightning_logs/version_1/checkpoints/epoch=199-step=77999.ckpt")
+    for b, d1, d2 in zip(baseline, dropit_fast_minik_08, dropit_fast_minik_099):
         writer.add_histogram(b, baseline[b], 0)
-        writer.add_histogram(b, pg4c[c], 1)
-        writer.add_histogram(b, pg4t[t], 2)
-        writer.add_histogram(b, pg4s[s], 3)
+        writer.add_histogram(b, dropit_fast_minik_08[d1], 1)
+        writer.add_histogram(b, dropit_fast_minik_099[d2], 2)
+        print(torch.norm((dropit_fast_minik_08[d1]-baseline[b]).float(), 2))
+        print(torch.norm((dropit_fast_minik_099[d2]-baseline[b]).float(), 2))
+        print("------------")
+
     writer.close()
